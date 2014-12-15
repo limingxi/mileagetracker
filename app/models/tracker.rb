20e420e8c254199c0998fec49fcb1d76de6cc983
limingxi <lee8568@gmail.com>
2014-12-15 12:41:30 -0600

    demo version

--- app/models/tracker.rb
+++ app/models/tracker.rb
@@ -1,4 +1,4 @@
 class Tracker < ActiveRecord::Base
-  attr_accessible :distance, :id, :timestamp, :employee_id
+  attr_accessible :distance, :id, :timestamp, :employee_id, :source, :dest
   belongs_to :employee
 end
